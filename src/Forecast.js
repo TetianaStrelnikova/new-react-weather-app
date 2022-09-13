65e650d8e3655cd11e317c08298d127f52842934
TetianaStrelnikova <strelnikova0108@gmail.com>
2022-09-13 19:38:13 +0400

    add forecast update

--- src/Forecast.js
+++ src/Forecast.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import axios from "axios";
 import { useState } from 'react';
 import DailyForecast from "./DailyForecast";
-
+import { useEffect } from 'react';
 export default function Forecast(props){
 
 const[ready, setReady]=useState(false);
 const [forecast, setForecast] = useState(null);
-
+useEffect(()=>{
+  setReady(false);},[props.lat, props.lon]
+)
 
 function handleResponse(response){
 setForecast(response.data.daily);
@@ -21,13 +23,9 @@ if (ready===true)
 {
   return(
 <div className='row pt-3 pb-3 ms-0 ps-1 pe-1 forecast'>
-{forecast.map(function(daily,index){if(index<5)return(<div className="col ps-0 pe-0" key={index}>
+{forecast.map(function(daily ,index){if(index<5){return(<div className="col ps-0 pe-0" key={index}>
   <DailyForecast  forecast={daily}/>
-</div>)})}
-
-
-
-
+</div>)}})}
 </div>
 )}
  
