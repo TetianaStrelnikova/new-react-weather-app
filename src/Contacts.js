18be9ba8d59e6f20b084b2a4920932745a64cd96
TetianaStrelnikova <strelnikova0108@gmail.com>
2022-09-13 20:53:57 +0400

    add github link

--- src/Contacts.js
+++ src/Contacts.js
@@ -8,7 +8,7 @@ return(
 <a className="linkedin" href="https://www.linkedin.com/in/tetiana-strelnikova-623459229/" target="blank" rel="noreferrer">
    <i className="fa-brands fa-linkedin"></i></a>
    <a className="instagram" href="https://www.instagram.com/tati_strelnik/" target="blank" rel="noreferrer"><i className="fa-brands fa-instagram-square"></i></a> 
-   <a className="githublink" href="https://github.com/TetianaStrelnikova/weather-app" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a>
+   <a className="githublink" href="https://github.com/TetianaStrelnikova/new-react-weather-app" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a>
 
 </div>
 )
